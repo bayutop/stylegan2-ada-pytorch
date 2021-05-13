5a503a00d557618e55d8d45b4d655a7d9c37e979
bayutop <82275180+bayutop@users.noreply.github.com>
2021-05-14 00:47:58 +0700

    Tambah copy file buat video

--- projector.py
+++ projector.py
@@ -185,6 +185,8 @@ def run_projection(
 
     # Render debug output: optional video and projected image and W vector.
     os.makedirs(outdir, exist_ok=True)
+    os.makedirs('projection_animation', exist_ok=True)
+    
     if save_video:
         video = imageio.get_writer(f'{outdir}/proj.mp4', mode='I', fps=10, codec='libx264', bitrate='16M')
         print (f'Saving optimization progress video "{outdir}/proj.mp4"')
@@ -192,6 +194,7 @@ def run_projection(
             synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
             synth_image = (synth_image + 1) * (255/2)
             synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
+            PIL.Image.fromarray(synth_image, 'RGB').save(f'projection_animation/{projected_w}.png')
             video.append_data(np.concatenate([target_uint8, synth_image], axis=1))
         video.close()
 
